2400cbb4ae2dc63cbbe532017e441df2a7dc3ca6
nitra0997 <31594535+nitra0997@users.noreply.github.com>
2018-05-12 11:40:37 +0200

    Parameterized Raw Types, Removed Non-JavaDoc comments

--- SERefactoring/src/Customer.java
+++ SERefactoring/src/Customer.java
@@ -1,17 +1,19 @@
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Enumeration;
-import java.util.Vector;
+import java.util.List;
 
 class Customer {
 	private String name;
-	private Vector rentals = new Vector();
+	private List<Rental> rentals = new ArrayList<Rental>();
 
 	public Customer(String newname) {
 		name = newname;
 	};
 
 	public void addRental(Rental arg) {
-		rentals.addElement(arg);
+		rentals.add(arg);
 	};
 
 	public String getName() {
@@ -21,26 +23,21 @@ class Customer {
 	public String statement() {
 		double totalAmount = 0;
 		int frequentRenterPoints = 0;
-		Enumeration enum_rentals = rentals.elements();
+		Enumeration<Rental> enum_rentals = Collections.enumeration(rentals);
 		String result = "Rental Record for " + this.getName() + "\n";
 		result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
 
 		while (enum_rentals.hasMoreElements()) {
 			double thisAmount = 0;
 			Rental each = (Rental) enum_rentals.nextElement();
-			// determine amounts for each line
 			thisAmount = amountFor(each);
-			// add frequent renter points
 			frequentRenterPoints++;
-			// add bonus for a two day new release rental
 			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
 				frequentRenterPoints++;
-			// show figures for this rental
 			result += "\t" + each.getMovie().getTitle() + "\t" + "\t" + each.getDaysRented() + "\t"
 					+ String.valueOf(thisAmount) + "\n";
 			totalAmount += thisAmount;
 		}
-		// add footer lines
 		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
 		result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
 		return result;
